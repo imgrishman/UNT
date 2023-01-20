9178d53a158c3963483a67879e8d3b0386001bb7
imgrishman <102344208+imgrishman@users.noreply.github.com>
2023-01-19 20:57:51 -0600

    Updated CSV data

--- sentiment.py
+++ sentiment.py
@@ -2,7 +2,7 @@ import pandas as pd
 from textblob import TextBlob
 
 # read the CSV file
-df = pd.read_csv(r'C:\Users\grish\OneDrive\Desktop\Task-1tweets_1000.csv', sep=',', names=['col1', 'col2', 'col3', 'col4', 'col5','col6','col7','col8'])
+df = pd.read_csv(r'https://github.com/imgrishman/UNT/blob/main/Task-1tweets_1000.csv', sep=',', names=['col1', 'col2', 'col3', 'col4', 'col5','col6','col7','col8'])
 
 
 # create a new column to store the sentiment scores
